3ea5cf18b36a3f4c7da823949ffb6c4bb4808b17
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-21 22:26:33 +0545

    Added Dalle Features and Routes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import dalleRoutes from './routes/dalle.route.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/v1/dalle',dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send({message:"Hi"})
--- routes/dalle.route.js
+++ routes/dalle.route.js
@@ -1,13 +1,36 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration,OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-    res.status(200).json({message:"Hello from dalle route"})
-})
+const config = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from dalle route" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: e.message });
+  }
+});
 
 export default router;
